2868de05d2b385ce6b2a6a41c9330df30057a589
LukeJessop <jessop.lucas@gmail.com>
2022-02-01 13:26:15 -0700

    minor change

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express()
 
 
 app.use(cors())
-app.use(express.static(`${__dirname} + /../build`))
+app.use(express.static(__dirname, '..' , '/../build'))
 app.use(express.json())
 
 app.use(session({
