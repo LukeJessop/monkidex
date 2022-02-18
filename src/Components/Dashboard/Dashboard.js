89ae1b550687cc1910ebbbb268d2fb1c22cb1614
LukeJessop <jessop.lucas@gmail.com>
2022-02-18 12:23:11 -0700

    created empty SQL files for comments, and fixed some UI on dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -24,35 +24,20 @@ class Dashboard extends Component {
 
     render(){
         let mappedPosts = this.state.allPosts.map((element) => {
+            console.log(element)
             return(
                 <div className="post-container">
-                <Link to={`/post/${element.post_id}`} key={element.post_id}>
-                    <div className="post">
-                        <div className="informationContainer">
-                            <div className="items">
-                                <div className="imgContainer">
-                                    <img className="imgPost" src={element.img_link}/>
-                                </div>
+                    <Link to={`/post/${element.post_id}`} key={element.post_id}>
+                        <div className="user-posts">
+                            <div className="user-info">
+                                <img className="profile-picture" src={element.profile_picture}/>
+                                <div className="username">{element.username}</div>
+                            </div>
+                            <div className="post-img-container">
+                                <img alt='post' className="img" src={element.img_link}/>
                             </div>
                         </div>
-                    </div>
-                </Link>
-                    <div className="profileContainer">
-                        <div className="userInfo">
-                            <img className='profilePicture' src={element.profile_picture}/>
-                            <h3>{element.username}</h3>
-                        </div>
-                        <div className="descriptionContainer">
-                            <div className="description">{element.description}</div>
-                        </div>
-                        <div className="interaction-buttons-container">
-                            <button className="like-button"></button>
-                            <button className="comment-button"></button>
-                            <div>
-                                <Comments/>
-                            </div>
-                        </div>
-                    </div>
+                    </Link>
                 </div>
             )
         })
